bdf8661ea6047fb284e5e7386a6061dbaef354b1
Heet Vakharia <heet1476@gmail.com>
2020-07-23 23:00:49 +0530

    added liking tweets funcnality

--- bot.js
+++ bot.js
@@ -90,7 +90,6 @@ const retweetInterval = () =>{
 const tweetInterval =() => {
 generatedTweetTime()
 }
-// tweet()
-// retweetInterval()
-// tweetInterval()
-likeAndRetweet()
+tweet()
+retweetInterval()
+tweetInterval()
