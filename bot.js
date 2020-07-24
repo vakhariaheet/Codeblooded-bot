3dcd6e1463be780f8cbe6f42c0ba9ba4b598b459
Heet Vakharia <heet1476@gmail.com>
2020-07-24 11:12:35 +0530

    retweets less tweet

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const likeAndRetweet = () => {
     const params = {
         q:'#100DaysOfCode',
         result_type : 'recent',
-        count:30
+        count:20
     }
     T.get('search/tweets',params,(err, data,response) => {
         let tweets = data.statuses
