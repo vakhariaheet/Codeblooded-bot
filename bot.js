21f7b2a2fde0f3ca00576bcc33cd2e765644ad01
Heet Vakharia <heet1476@gmail.com>
2020-09-13 23:56:38 +0530

    Added environment varibles

--- bot.js
+++ bot.js
@@ -2,95 +2,98 @@ import Twit from "twit";
 import config from "./config.js";
 const T = new Twit(config);
 import axios from "axios";
-// Retweet and likes lastest 10 tweets contaning #100DaysOfCode when run
+import dotenv from "dotenv";
+
+dotenv.config();
+
+Retweet and likes lastest 10 tweets contaning #100DaysOfCode when run
 const likeAndRetweet = () => {
-    const paramsN = {
-        q:'#100DaysOfCode',
-        result_type : 'recent',
-        count:20
+  const paramsN = {
+    q: "#100DaysOfCode",
+    result_type: "recent",
+    count: 5,
+  };
+  T.get("search/tweets", paramsN, (err, data, response) => {
+    let tweets = data.statuses;
+    if (!err) {
+      for (let dat of tweets) {
+        let retweetId = dat.id_str;
+        T.post("statuses/retweet/:id", { id: retweetId }, (err, response) => {
+          if (!err) {
+            console.log(`retweeted ${retweetId}`);
+            T.post("favorites/create", { id: retweetId }, (err, response) => {
+              if (!err) {
+                console.log(`liked ${retweetId}`);
+              } else {
+                console.log(`something went wrong ${err}`);
+              }
+            });
+          } else {
+            console.log(`something went wrong ${err}`);
+          }
+        });
+      }
+    } else {
+      console.log(err);
     }
-    T.get('search/tweets',paramsN,(err, data,response) => {
-        let tweets = data.statuses
-        if(!err){
-                for(let dat of tweets){
-                let retweetId = dat.id_str;
-                T.post('statuses/retweet/:id',{id: retweetId} , (err,response) => {
-                    if(!err){
-                        console.log(`retweeted ${retweetId}`)
-                        T.post('favorites/create',{id: retweetId} , (err,response) => {
-                            if(!err){
-                                console.log(`liked ${retweetId}`)
-                            }
-                            else{
-                                console.log(`something went wrong ${err}`)
-                            }
-                        })
-                    }
-                    else{
-                        console.log(`something went wrong ${err}`)
-                    }
-                })
-                
-                
-            }
-        }
-        else{
-            console.log(err)
-        }
-    })
-}
+  });
+};
 //Get a quote from the API and the Tweet it when run
 const tweet = () => {
-    axios({
-        "method":"GET",
-        "url":"https://quotes15.p.rapidapi.com/quotes/random/",
-        "headers":{
-        "content-type":"application/octet-stream",
-        "x-rapidapi-host":"quotes15.p.rapidapi.com",
-        "x-rapidapi-key":"59d0b2c3c0msh86e340ae9c71bb8p13d7cbjsn0c90670232b6",
-        "useQueryString":true
-        },"params":{
-        "language_code":"en"
-        }
-        })
-        .then((response)=>{
-            console.log(response.data)
-            let text = response.data.content,
-                author = response.data.originator.name;
-            let finalTweetMsg =  `${text}\n -${author}`;
-            T.post('statuses/update', { status: finalTweetMsg }, function(err, data, response) {
-                console.log(text,author)
-                })
-        })
-        .catch((error)=>{
-          console.log(error)
-        })
-
+  axios({
+    method: "GET",
+    url: "https://quotes15.p.rapidapi.com/quotes/random/",
+    headers: {
+      "content-type": "application/octet-stream",
+      "x-rapidapi-host": "quotes15.p.rapidapi.com",
+      "x-rapidapi-key": "59d0b2c3c0msh86e340ae9c71bb8p13d7cbjsn0c90670232b6",
+      useQueryString: true,
+    },
+    params: {
+      language_code: "en",
+    },
+  })
+    .then((response) => {
+      console.log(response.data);
+      let text = response.data.content,
+        author = response.data.originator.name;
+      let finalTweetMsg = `${text}\n -${author}`;
+      T.post("statuses/update", { status: finalTweetMsg }, function (
+        err,
+        data,
+        response
+      ) {
+        console.log(text, author);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
 //Randomly time in mins and at that time runs retweet Func. and retweetInterval func when run
 const generatedRetweetTime = () => {
-    let time = 60000 * Math.floor((Math.random() *10)+1)
-    console.log(time,'retweet')
-    setTimeout(()=>{
-        likeAndRetweet()
-        retweetInterval()
-    } , time)
-}
+  let time = 60000 * Math.floor(Math.random() * 10 + 1);
+  console.log(time, "retweet");
+  setTimeout(() => {
+    likeAndRetweet();
+    retweetInterval();
+  }, time);
+};
 const generatedTweetTime = () => {
-    let timeForQoute = 60000 * Math.floor((Math.random() *5)+1)
-    console.log(timeForQoute,'Quote')
-    setTimeout(()=>{
-       tweet()
-       tweetInterval()
-    } , timeForQoute)
-}
-const retweetInterval = () =>{
-    generatedRetweetTime()
-}
-const tweetInterval =() => {
-generatedTweetTime()
-}
-likeAndRetweet()
-retweetInterval()
-tweetInterval()
+  let timeForQoute = 60000 * Math.floor(Math.random() * 3 + 1);
+  console.log(timeForQoute, "Quote");
+  setTimeout(() => {
+    tweet();
+    tweetInterval();
+  }, timeForQoute);
+};
+const retweetInterval = () => {
+  generatedRetweetTime();
+};
+const tweetInterval = () => {
+  generatedTweetTime();
+};
+likeAndRetweet();
+retweetInterval();
+tweetInterval();
--- config.js
+++ config.js
@@ -1,8 +1,15 @@
+const {
+  consumer_key,
+  consumer_secret,
+  access_token,
+  access_token_secret,
+} = process.env;
+
 const config = {
-  consumer_key: "PL1U6S6f0DrLZzxyBqcptaWCR",
-  consumer_secret: "2bXSI7gHoH8tZ0SWdGuXjq8JDaFavKynfo2ORGtoaezmeXoMR4",
-  access_token: "1287306574998904832-rHbCCL2VL17moBL3DXfbObMnjiuj8Z",
-  access_token_secret: "UVdc3ApY6xFEpRYKAJbNbIUtZ6CbkSzeAWbqHSLvvbZLN",
+  consumer_key,
+  consumer_secret,
+  access_token,
+  access_token_secret,
   timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
   strictSSL: true, // optional - requires SSL certificates to be valid.
 };
